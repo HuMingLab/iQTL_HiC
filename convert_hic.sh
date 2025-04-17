2848284f82316e23ebf518a7a90ea2d568e19976
lindsayhrlee <71450689+lindsayhrlee@users.noreply.github.com>
2025-04-17 09:22:06 -0400

    Update convert_hic.sh

--- convert_hic.sh
+++ convert_hic.sh
@@ -3,9 +3,9 @@
 CC1=$1
 CC2=$2
 name=$3
+out_dir=$4
 
-
-cd ../${CC1}x${CC2}_${name}
+cd $out_dir
 
 for((i=1;i<3;i++)); 
 do 
@@ -15,18 +15,18 @@ grep -v '#' test${i}.sorted.pairs | awk '{if ($2 > $4){print $6"\t"$2"\t"$3"\t""
 sed 's/+/0/g' tmp.pairs > test.pairs;
 sed -i 's/-/1/g' test.pairs;
 sort -k2,2d -k6,6d test.pairs > tmp.pairs;
-java -Xmx100g -jar /home/leeh7/software/juicer_tools_1.22.01.jar pre -r 10000 tmp.pairs ${name}.phase_allele${i}.hic /home/leeh7/iQTL/fasta/chr_size/${CC1}x${CC2}_chr_sizes.txt;
+java -Xmx100g -jar juicer_tools_1.22.01.jar pre -r 10000 tmp.pairs ${name}.phase_allele${i}.hic /home/leeh7/iQTL/fasta/chr_size/${CC1}x${CC2}_chr_sizes.txt;
 rm -rf test${i}.sorted.pairs test.pairs tmp.pairs; done
 
 
-for((j=1;j<20;j++)); do java -Xmx20g -jar /home/leeh7/software/juicer_tools_1.22.01.jar dump observed NONE ${name}.phase_allele1.hic chr${j}_${CC1} chr${j}_${CC1} BP 10000 ${name}.phase_allele1_chr${j}.txt; done
-for((j=1;j<20;j++)); do java -Xmx20g -jar /home/leeh7/software/juicer_tools_1.22.01.jar dump observed NONE ${name}.phase_allele2.hic chr${j}_${CC2} chr${j}_${CC2} BP 10000 ${name}.phase_allele2_chr${j}.txt; done
+for((j=1;j<20;j++)); do java -Xmx20g -jar juicer_tools_1.22.01.jar dump observed NONE ${name}.phase_allele1.hic chr${j}_${CC1} chr${j}_${CC1} BP 10000 ${name}.phase_allele1_chr${j}.txt; done
+for((j=1;j<20;j++)); do java -Xmx20g -jar juicer_tools_1.22.01.jar dump observed NONE ${name}.phase_allele2.hic chr${j}_${CC2} chr${j}_${CC2} BP 10000 ${name}.phase_allele2_chr${j}.txt; done
 
 for((i=1;i<3;i++)); do
 for((j=1;j<20;j++)); do awk -F $'\t' -v a="chr${j}" '{print a,$0}' OFS=$'\t' ${name}.phase_allele${i}_chr${j}.txt > ${name}.phase_allele${i}_chr${j}_fixed.txt; done;
 
 cat ${name}.phase_allele${i}_chr*_fixed.txt > all_${name}.phase_allele${i}_counts.txt; done
-cd ..
+
 
 
 

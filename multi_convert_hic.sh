9834b47fecd4c7e7b97482dc60f91fb7b0d16a6c
HuMingLab <39558841+HuMingLab@users.noreply.github.com>
2025-04-24 09:56:09 -0700

    Update multi_convert_hic.sh

--- multi_convert_hic.sh
+++ multi_convert_hic.sh
@@ -3,13 +3,12 @@
 CC1=$1
 CC2=$2
 name=$3
-out_dir=$4
 
 
 CCs=(${CC1} ${CC2}
 )
 
-cd $out_dir
+cd 
 
 for CC in "${CCs[@]}" ; do 
 pairtools parse -c /home/leeh7/iQTL/fasta/chr_size/${CC}_chr_sizes.txt --drop-sam ${CC}_${name}.bam -o ${CC}_${name}.pairs
@@ -18,11 +17,11 @@ grep -v '#' test${CC}.sorted.pairs | awk '{if ($2 > $4){print $6"\t"$2"\t"$3"\t"
 sed 's/+/0/g' tmp.pairs > test.pairs;
 sed -i 's/-/1/g' test.pairs;
 sort -k2,2d -k6,6d test.pairs > tmp.pairs;
-java -Xmx100g -jar juicer_tools_1.22.01.jar pre -r 10000 tmp.pairs ${CC}_${name}.hic /home/leeh7/iQTL/fasta/chr_size/${CC}_chr_sizes.txt;
+java -Xmx100g -jar /home/leeh7/software/juicer_tools_1.22.01.jar pre -r 10000 tmp.pairs ${CC}_${name}.hic /home/leeh7/iQTL/fasta/chr_size/${CC}_chr_sizes.txt;
 rm -rf test${CC}.sorted.pairs test.pairs tmp.pairs;
 
 
-for((j=1;j<20;j++)); do java -Xmx20g -jar juicer_tools_1.22.01.jar dump observed NONE ${CC}_${name}.hic chr${j}_${CC} chr${j}_${CC} BP 10000 ${CC}_${name}_chr${j}.txt; done;
+for((j=1;j<20;j++)); do java -Xmx20g -jar /home/leeh7/software/juicer_tools_1.22.01.jar dump observed NONE ${CC}_${name}.hic chr${j}_${CC} chr${j}_${CC} BP 10000 ${CC}_${name}_chr${j}.txt; done;
 
 for((j=1;j<20;j++)); do awk -F $'\t' -v a="chr${j}" '{print a,$0}' OFS=$'\t' ${CC}_${name}_chr${j}.txt > ${CC}_${name}_chr${j}_fixed.txt; done;
 
@@ -34,7 +33,3 @@ cat ${CC}_${name}_chr*_fixed.txt > all_${CC}_${name}_counts.txt; done
 
 
 
-#for((i=1;i<20;i++)); do java -Xmx20g -jar /home/leeh7/software/juicer_tools_1.22.01.jar dump observed NONE MH213.phase_allele2.hic chr${i}_CC021 chr${i}_CC021 BP 10000 MH213.phase_allele2_chr${i}.txt; done
-
-#for((i=1;i<20;i++)); do awk -F $'\t' -v a="chr${i}_CC075" '{print a,$0}' OFS=$'\t' MH213.phase_allele1_chr${i}.txt > MH213.phase_allele1_chr${i}_fixed.txt; done
-#for((i=1;i<20;i++)); do awk -F $'\t' -v a="chr${i}_CC021" '{print a,$0}' OFS=$'\t' MH213.phase_allele2_chr${i}.txt > MH213.phase_allele2_chr${i}_fixed.txt; done
